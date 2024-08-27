3c21071efc55a81a0806aeafa5b193640da5d124
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-08-27 18:00:32 -0300

    Projeto API Pedido iniciado

---
+++ src/main/java/com/kata/pedidos/ApiPedidoRecepcaoApplication.java
@@ -0,0 +1,13 @@
+package com.kata.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPedidoRecepcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPedidoRecepcaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/pedidos/ApiPedidoRecepcaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPedidoRecepcaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
